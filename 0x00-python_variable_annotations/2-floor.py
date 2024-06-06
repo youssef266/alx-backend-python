8808f779b1f04e55f889643941fe4d6796b8ddcd
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-06-06 20:32:58 +0300

    alx

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,9 +3,8 @@
 Write a type-annotated function concat that takes a string
 str1 and a string str2 as arguments and returns a concatenated string
 """
-
-
 import math
+
 
 def floor(n: float) -> int:
     """return concatenated string"""
