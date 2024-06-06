959735b433fd3b413f58430b13a64bdfca9616fc
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-06-06 20:28:33 +0300

    alx

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function concat that takes a string
+str1 and a string str2 as arguments and returns a concatenated string
+"""
+
+
+import math
+
+def floor(n: float) -> int:
+    """return concatenated string"""
+    return math.floor(n)
