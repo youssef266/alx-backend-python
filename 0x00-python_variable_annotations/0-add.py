aa366c3590df11cdb80ec86a6f9e8a4a16380770
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-06-06 19:43:39 +0300

    alx

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+This module contains the function add that takes
+two float arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    return a + b
