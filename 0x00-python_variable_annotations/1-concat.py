49210655ff367deaba5d788e6a539e2d37c68ac5
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-06-06 19:48:48 +0300

    alx

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module contains the function concat that takes
+two string arguments and returns their concatenation.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of two strings."""
+    return str1 + str2
